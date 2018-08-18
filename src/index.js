85ec4007f8aa99abb9581e1c1b1ef950ae0ac518
minggas <fasfora@gmail.com>
2018-08-18 20:21:56 -0300

    delete gitribbon component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom";
 import "./scss/main.scss";
 import App from "./App";
 import Footer from "./components/Footer";
-import GitRibbon from "./components/GitRibbon";
 //import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <div>
-    <GitRibbon />
     <App />
     <Footer />
   </div>,
