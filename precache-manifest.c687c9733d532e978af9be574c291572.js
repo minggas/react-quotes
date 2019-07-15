f26bee3bc01321a1e60aa5c95596c10b3bdcecd9
minggas <fasfora@gmail.com>
2019-07-15 20:20:47 -0300

    Updates

---
+++ precache-manifest.c687c9733d532e978af9be574c291572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fd94adfb3a6ea5a626ab393e5a315e",
+    "url": "/react-quotes/index.html"
+  },
+  {
+    "revision": "ff26eb0f8b180c3c4e0b",
+    "url": "/react-quotes/static/css/main.ba19c120.chunk.css"
+  },
+  {
+    "revision": "652e691c10dd16569187",
+    "url": "/react-quotes/static/js/2.8b429a8f.chunk.js"
+  },
+  {
+    "revision": "ff26eb0f8b180c3c4e0b",
+    "url": "/react-quotes/static/js/main.cd7d4c90.chunk.js"
+  },
+  {
+    "revision": "74c8f628b380deddc767",
+    "url": "/react-quotes/static/js/runtime~main.e47a8ed2.js"
+  }
+]);
